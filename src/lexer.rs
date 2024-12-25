b16a2e1354c56b4a2cd37f8301dd5a30cfde5264
Km1zZzoU <zhovnir.ea@gmail.com>
2024-12-26 04:45:42 +0700

    lexer 0.0

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,47 +1,113 @@
-enum Token {
+#[derive(Debug, PartialEq)]
+pub enum Token {
     Number(i64),
+
+    Sum,
+    Plus,
+    Mul,
+    And,
+    Or,
+    Xor,
+    Not,
+    LogAnd,
+    LogOr,
+
     Assign,
     Declaration,
+
     Variable(String),
+    Function(String),
+
+    LeftBracket,
+    RightBracket,
+
+    Dot,
+    Comma,
+
+    If,
+    Else,
+    For,
+    Fnc,
+    Ret,
+
+    End,
 }
 
+struct Context {
+    startoftoken: usize,
+    canbenumber: bool,
+    canbevar: bool,
+    index: usize,
+    tokens: Vec<Token>,
+}
+
+impl Context {
+    pub fn parse(&mut self, string: &str, newsubstr: &str) {
+        let strofoldtoken: String = string[self.startoftoken..self.index].to_string();
+        if self.canbenumber {
+            self.tokens
+                .push(Token::Number(strofoldtoken.parse::<i64>().unwrap()));
+        } else if self.canbevar {
+            self.tokens.push(Token::Variable(strofoldtoken));
+        } else {
+            self.tokens.push(Token::Function(strofoldtoken));
+        }
+        self.index += newsubstr.len() - 1;
+        self.canbenumber = true;
+        self.canbevar = false;
+        self.startoftoken = self.index + 1;
+    }
+}
+const IF: &str = "if";
+const DECL: &str = ":=";
+const ASSIGN: &str = "=";
+const END: &str = ";";
+
+//qwe123:=45;
+//0123456789
 pub fn lex(str: &str) -> Vec<Token> {
-    let mut tokens = Vec::new();
+    let mut cntxt = Context {
+        startoftoken: 0,
+        canbevar: false,
+        canbenumber: true,
+        index: 0,
+        tokens: Vec::new(),
+    };
 
-    let startoftoken = 0;
-    let mut canbenumber = false;
-    let mut canbevar = false;
-
-    for mut i in 0..str.len() {
-        let c = str.chars().nth(i).unwrap();
+    while cntxt.index < str.len() {
+        let c = str.chars().nth(cntxt.index).unwrap();
         match c {
             _ if c.is_ascii_alphabetic() => {
-                canbenumber = false;
-                canbevar = true;
+                cntxt.canbenumber = false;
+                cntxt.canbevar = true;
             }
-            ':' => if str.chars().nth(i + 1).unwrap() == '=' {
-                if canbevar {
-                    tokens.push(Token::Variable(str[startoftoken..i].to_string()));
-                } else {
-                    panic!("number can not be Declareted")
+            _ if c.is_ascii_digit() => {
+                cntxt.index += 1;
+                continue;
+            }
+            ' ' => {
+                cntxt.canbenumber = false;
+                cntxt.canbevar = false;
+            }
+            ';' => {
+                if cntxt.index != cntxt.startoftoken {
+                    cntxt.parse(str, END);
                 }
-                tokens.push(Token::Declaration);
-                i += 1;
-                canbenumber = true;
-                canbevar = true;
+                cntxt.tokens.push(Token::End)
             }
-            '=' => {
-                if canbevar {
-                    tokens.push(Token::Variable(str[startoftoken..i].to_string()));
-                } else {
-                    panic!("number can not be Assigned");
+            _ => match str[cntxt.index..].to_string() {
+                s if s.starts_with(DECL) => {
+                    cntxt.parse(str, DECL);
+                    cntxt.tokens.push(Token::Declaration);
                 }
-                tokens.push(Token::Assign);
-                canbenumber = true;
-                canbevar = true;
+                s if s.starts_with(ASSIGN) => {
+                    cntxt.parse(str, ASSIGN);
+                    cntxt.tokens.push(Token::Assign);
+                }
+                _ => {}
             }
-            _ => {}
         }
+        cntxt.index += 1;
     }
-    tokens
+    cntxt.tokens
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+mod lexer;
+mod parser;
+
+use lexer::Token;
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_lex() {
+        let input = "qwe123:=42;";
+        let expected_tokens = vec![
+            Token::Variable("qwe123".to_string()),
+            Token::Declaration,
+            Token::Number(42),
+            Token::End
+        ];
+
+        let tokens = lexer::lex(input);
+
+        assert_eq!(tokens.len(), expected_tokens.len(), "Token lengths don't match.");
+
+        for (i, (token, expected_token)) in tokens.iter().zip(expected_tokens.iter()).enumerate() {
+            assert_eq!(token, expected_token, "Token mismatch at index {}", i);
+        }
+    }
 }
+
