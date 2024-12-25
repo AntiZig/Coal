39a697f00a934529854ceb4abf8d12606c78dbc4
Km1zZzoU <zhovnir.ea@gmail.com>
2024-12-25 20:25:29 +0700

    init

---
+++ target/debug/Coal.d
@@ -0,0 +1,1 @@
+/home/ezhe/docs/prog_sys/Coal/target/debug/Coal: /home/ezhe/docs/prog_sys/Coal/src/main.rs
---
+++ target/debug/deps/Coal-ad11ee0f738bf7c0.d
@@ -0,0 +1,5 @@
+/home/ezhe/docs/prog_sys/Coal/target/debug/deps/Coal-ad11ee0f738bf7c0: src/main.rs
+
+/home/ezhe/docs/prog_sys/Coal/target/debug/deps/Coal-ad11ee0f738bf7c0.d: src/main.rs
+
+src/main.rs:
