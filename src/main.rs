91dfc52b2bd537c54920698b32bf4929f0044675
rendemare <rendemare@gmail.com>
2024-12-25 23:19:50 +0700

    a little better

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,7 @@
-use std::env::{args, Args};
-
-use startup::read_file;
-
 mod lexer;
 mod startup;
 
-#[derive(Debug)]
-struct ArgsParserError {
-    error: String,
-}
-
-fn getPath(args: Args) -> Result<String, ArgsParserError> {
-    let args: Vec<String> = args.collect();
-    if args.len() != 2 {
-        return Err(ArgsParserError {
-            error: String::from("invalid arguments"),
-        });
-    }
-    Ok(args[1].clone())
-}
-
 fn main() {
-    let path = getPath(args()).unwrap();
-    println!("{}", read_file(path).unwrap());
+    let code = startup::get_code();
+    println!("{}", code);
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,38 @@
+use std::env::args;
 use std::fs::File;
 use std::io::{Error, Read};
 
-pub fn read_file(path: String) -> Result<String, Error> {
+#[derive(Debug)]
+struct ArgsParserError {
+    error_message: String,
+}
+
+pub fn get_code() -> String {
+    let args = args().collect();
+    let path_result = get_path(args);
+    let path = match path_result {
+        Ok(str) => str,
+        Err(args_error) => {
+            panic!("{}", args_error.error_message);
+        }
+    };
+    let file_result = read_file(path);
+    match file_result {
+        Ok(file) => file,
+        Err(err) => panic!("{}", err),
+    }
+}
+
+fn get_path(args: Vec<String>) -> Result<String, ArgsParserError> {
+    if args.len() != 2 {
+        return Err(ArgsParserError {
+            error_message: String::from("invalid arguments"),
+        });
+    }
+    Ok(args.last().unwrap().clone())
+}
+
+fn read_file(path: String) -> Result<String, Error> {
     let mut file = File::open(path)?;
     let mut str = String::new();
     file.read_to_string(&mut str)?;
