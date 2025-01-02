7acd10c36baf8e1a85ed2be14729f98dbf51a41a
rendemare <rendemare@gmail.com>
2025-01-02 21:11:46 +0700

    added support for function calls. also changed structure.

--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -1,8 +1,14 @@
+mod ir_types;
+
+use ir_types::{IRBlock, IROp, IRValue, IRctx, IReg};
+
 use core::fmt;
 use std::{
     collections::HashMap,
     fmt::{Debug, Display, Formatter, Pointer},
 };
+
+type Type = String;
 
 #[derive(Debug)]
 enum Expr {
@@ -22,8 +28,6 @@ enum Stmt {
     If(Expr, Vec<Stmt>),
 }
 
-type Type = String;
-
 #[derive(Debug)]
 struct Fn {
     name: String,
@@ -32,131 +36,9 @@ struct Fn {
     body: Vec<Stmt>,
 }
 
-#[derive(Debug)]
-struct IRBlock {
-    name: String,
-    ttype: Type,
-    params: Vec<Type>,
-    ctx: IRctx,
-}
-
-impl ToString for IRBlock {
-    fn to_string(&self) -> String {
-        let name = &self.name;
-        let t = &self.ttype;
-
-        let header = String::from("block ") + &name + &String::from(" ") + &t + &String::from("\n");
-        let code = self
-            .ctx
-            .ops
-            .clone()
-            .into_iter()
-            .enumerate()
-            .fold("".to_string(), |acc, (i, op)| {
-                acc + &op.to_string_triple(i) + &String::from("\n")
-            });
-        header + &code
-    }
-}
-
-type IReg = usize;
-
-#[derive(Debug, Clone)]
-enum IROp {
-    Load(IRValue),
-    Mov(IReg),
-    Store(IReg),
-    Add(IReg, IReg),
-    Sub(IReg, IReg),
-    Mul(IReg, IReg),
-}
-
-impl IROp {
-    fn to_string_triple(&self, i: usize) -> String {
-        match self {
-            Self::Mov(reg) => {
-                String::from("mov ") + &reg_to_str(i) + &String::from(", ") + &reg_to_str(*reg)
-            }
-            Self::Store(reg) => {
-                String::from("str ") + &reg_to_str(i) + &String::from(", ") + &reg_to_str(*reg)
-            }
-            Self::Add(r1, r2) => {
-                String::from("add ")
-                    + &reg_to_str(i)
-                    + &String::from(", ")
-                    + &reg_to_str(*r1)
-                    + &String::from(", ")
-                    + &reg_to_str(*r2)
-            }
-            Self::Sub(r1, r2) => {
-                String::from("sub ")
-                    + &reg_to_str(i)
-                    + &String::from(", ")
-                    + &reg_to_str(*r1)
-                    + &String::from(", ")
-                    + &reg_to_str(*r2)
-            }
-            Self::Mul(r1, r2) => {
-                String::from("mul ")
-                    + &reg_to_str(i)
-                    + &String::from(", ")
-                    + &reg_to_str(*r1)
-                    + &String::from(", ")
-                    + &reg_to_str(*r2)
-            }
-            Self::Load(val) => {
-                String::from("ld ") + &reg_to_str(i) + &String::from(", ") + &val.to_string()
-            }
-            _ => unimplemented!(),
-        }
-    }
-}
-
-impl ToString for IROp {
-    fn to_string(&self) -> String {
-        match self {
-            Self::Mov(reg) => String::from("mov ") + &reg_to_str(*reg),
-            Self::Store(reg) => String::from("str ") + &reg_to_str(*reg),
-            Self::Add(r1, r2) => {
-                String::from("add ") + &reg_to_str(*r1) + &String::from(" ") + &reg_to_str(*r2)
-            }
-            Self::Sub(r1, r2) => {
-                String::from("sub ") + &reg_to_str(*r1) + &String::from(" ") + &reg_to_str(*r2)
-            }
-            Self::Mul(r1, r2) => {
-                String::from("mul ") + &reg_to_str(*r1) + &String::from(" ") + &reg_to_str(*r2)
-            }
-            Self::Load(val) => String::from("ld ") + &val.to_string(),
-            _ => unimplemented!(),
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-enum IRValue {
-    Reg(IReg),
-    U64(u64),
-    Var(String),
-}
-
-fn reg_to_str(reg: IReg) -> String {
-    "r".to_string() + &reg.to_string()
-}
-
-impl ToString for IRValue {
-    fn to_string(&self) -> String {
-        match self {
-            IRValue::Reg(register) => reg_to_str(*register),
-            IRValue::U64(n) => n.to_string(),
-            IRValue::Var(str) => str.to_string(),
-        }
-    }
-}
-
 fn fn_to_ir(func: Fn) -> IRBlock {
     let mut block = IRBlock {
-        name: func.name,
-        ttype: func.ttype,
+        label: func.name,
         params: func
             .params
             .iter()
@@ -165,6 +47,7 @@ fn fn_to_ir(func: Fn) -> IRBlock {
         ctx: IRctx {
             ops: Vec::new(),
             vtoi: HashMap::new(),
+            lcounter: 0,
         },
     };
     func.body
@@ -185,7 +68,7 @@ fn stmt_to_ir(stmt: Stmt, ctx: &mut IRctx) -> IReg {
 
 fn declare_to_ir(var: String, expr: Expr, ctx: &mut IRctx) -> IReg {
     let r1 = expr_to_ir(expr, ctx);
-    let op = IROp::Store(r1);
+    let op = IROp::Store(r1, String::from("store into ".to_string() + &var));
     ctx.ops.push(op);
     let reg = ctx.ops.len() - 1;
     // TODO: name collisions, local name for each block etc.
@@ -195,7 +78,7 @@ fn declare_to_ir(var: String, expr: Expr, ctx: &mut IRctx) -> IReg {
 
 fn assign_to_ir(var: String, expr: Expr, ctx: &mut IRctx) -> IReg {
     let r1 = expr_to_ir(expr, ctx);
-    let op = IROp::Store(r1);
+    let op = IROp::Store(r1, String::from("store into ".to_string() + &var));
     ctx.ops.push(op);
     let reg = ctx.ops.len() - 1;
     // TODO: check if there is no variable by that name yet, crash.
@@ -212,14 +95,9 @@ fn expr_to_ir(e: Expr, ctx: &mut IRctx) -> IReg {
         Expr::Mul(e1, e2) => mul_to_ir(e1, e2, ctx),
         Expr::Num(n) => num_to_ir(n, ctx),
         Expr::Name(str) => var_to_ir(str, ctx),
+        Expr::FuncCall(label, exprs) => fcall_to_ir(label, exprs, ctx),
         _ => unimplemented!(),
     }
-}
-
-#[derive(Debug)]
-struct IRctx {
-    ops: Vec<IROp>,
-    vtoi: HashMap<String, usize>,
 }
 
 // TODO: generalize next 3 functions
@@ -254,13 +132,23 @@ fn num_to_ir(num: u64, ctx: &mut IRctx) -> IReg {
     ctx.ops.len() - 1
 }
 
+fn fcall_to_ir(label: String, exprs: Vec<Expr>, ctx: &mut IRctx) -> IReg {
+    let regs: Vec<IReg> = exprs
+        .into_iter()
+        .map(|expr| expr_to_ir(expr, ctx))
+        .collect();
+    let op = IROp::Call(label, regs);
+    ctx.ops.push(op);
+    ctx.ops.len() - 1
+}
+
 fn var_to_ir(var: String, ctx: &mut IRctx) -> IReg {
     let index = match ctx.vtoi.get(&var) {
         Some(k) => k,
         None => panic!("use of variable before declaration."),
     };
 
-    let mov = IROp::Mov(*index);
+    let mov = IROp::Mov(*index, String::from("moved from ") + &var);
     ctx.ops.push(mov);
     ctx.ops.len() - 1
 }
@@ -284,12 +172,17 @@ mod tests {
             )),
             Box::new(Expr::Num(32)),
         );
+        let expr3 = Expr::FuncCall(
+            "function".to_string(),
+            Vec::from([Expr::Num(42), Expr::Name("a".to_string())]),
+        );
         let stmt2 = Stmt::Assign("a".to_string(), expr2);
+        let stmt3 = Stmt::Declare("ret".to_string(), expr3);
         Fn {
             name: String::from("test function"),
             ttype: String::from("kek"),
             params: Vec::from([(String::from("lol"), String::from("kekington"))]),
-            body: Vec::from([stmt1, stmt2]),
+            body: Vec::from([stmt1, stmt2, stmt3]),
         }
     }
 
